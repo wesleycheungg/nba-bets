a5d14a9904a4d2634e04ccf0754e072e63cb5a81
wesley <cheung.w.2016@gmail.com>
2024-01-24 10:08:11 -0800

    update oddsApiKey in keys_development file

--- config/keys_development.js
+++ config/keys_development.js
@@ -1,5 +1,5 @@
 module.exports = {
     mongoURI: "mongodb+srv://OwenWinzeler:tFPkaG3r22yCFjM@cluster0.qqhxl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
     secretOrKey: "secret",
-    oddsApiKey: "ed31e7e8b0f17680852a0dea56042e71"
+    oddsApiKey: "8af76d48b9e543989c6aba54a53b8c37"
 }
