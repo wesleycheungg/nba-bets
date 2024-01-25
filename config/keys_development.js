562e35084eb34dba4b66830f5569c571396c68b4
wesley <cheung.w.2016@gmail.com>
2024-01-24 21:49:23 -0800

    .gitignore is now working

--- config/keys_development.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    // mongoURI: "mongodb+srv://OwenWinzeler:tFPkaG3r22yCFjM@cluster0.qqhxl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-    mongoURI: "mongodb+srv://wesleyhcheung:mtxYi8X3dghqUlqd@nba-bets-cluster-1.naoeydi.mongodb.net/retryWrites=true&w=majority",
-    secretOrKey: "secret",
-    oddsApiKey: "8af76d48b9e543989c6aba54a53b8c37"
-}
